801238a17ce4332deb2dabcdfe8d9727eb1cb4c1
suzuim <p19028is@gm.tsuda.ac.jp>
2021-12-27 18:30:48 +0900

    Initial Commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by 今井涼葉 on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
